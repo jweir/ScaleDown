bf9b901df04d87e4b9b403b4f3ec74ae1289fa96
J. Weir <john@smokinggun.com>
2010-11-05 17:50:51 -0400

    Documented CGI escaping filenames

--- test/scaled_image_tag_test.rb
+++ test/scaled_image_tag_test.rb
@@ -1,10 +1,12 @@
 require File.expand_path(File.dirname(__FILE__))+'/test_helper'
 
+require 'cgi'
+
 class ScaleDown::Test < Test::Unit::TestCase
 
-  def scaled_image_src(url)
-    hmac = HMAC::SHA1.new("secret").update(url).to_s[0...8]
-    "http://images.myserver.com#{url}/#{hmac}"
+  def signed_image_url(path, filename, options)
+    hmac = HMAC::SHA1.new("secret").update([path, filename, options].join("/")).to_s[0...8]
+    "http://images.myserver.com#{[path, CGI.escape(filename), options, hmac].join("/")}"
   end
 
   setup do
@@ -16,6 +18,16 @@ class ScaleDown::Test < Test::Unit::TestCase
 
   should "create a URL with the HMAC signature" do
     hmac = ScaleDown.hmac("/images/graphic.png/400x300-cropped")
-    assert_equal "http://images.myserver.com/images/graphic.png/400x300-cropped/#{hmac}", scaled_image_src("/images/graphic.png/400x300-cropped")
+    assert_equal\
+      "http://images.myserver.com/images/graphic.png/400x300-cropped/#{hmac}",
+      signed_image_url("/images","graphic.png","400x300-cropped")
+  end
+
+  should "create a URL when the filename has URI break characters" do
+    filename = "# !%23?.png"
+    hmac = ScaleDown.hmac("/images/#{filename}/400x300-cropped")
+    assert_equal\
+      "http://images.myserver.com/images/#{CGI.escape filename}/400x300-cropped/#{hmac}",
+      signed_image_url("/images", filename, "400x300-cropped")
   end
 end
